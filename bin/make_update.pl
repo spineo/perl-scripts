d68a48a7469bb40cc1d031ba25e7934fa825d357
Stuart Pineo <svpineo@gmail.com>
2017-01-12 08:51:22 -0500

    Did away with the --db-file command-line option replaced by --db-dir. Added db name and extension to the configuration. Refs #E254.

--- bin/make_update.pl
+++ bin/make_update.pl
@@ -3,7 +3,7 @@
 #------------------------------------------------------------------------------
 # Name       : make_update.pl
 # Author     : Stuart Pineo  <svpineo@gmail.com>
-# Usage      : ./make_update.pl --db-file "path to db file" 
+# Usage      : ./make_update.pl --db-dir "path to db directory" 
 #                             [ --version-file "version file" ]
 # Description: Create a new App database update, deploy it to GitHub, 
 # and trigger the Jenkins build.
@@ -37,7 +37,7 @@ use Carp qw(croak carp);
 use Data::Dumper;
 use File::Copy qw(copy);
 use Cwd;
-use Util::GenericUtils qw(isFile isPath trim_all);
+use Util::GenericUtils qw(isFile isPath trim);
 
 # Global variables
 #
@@ -51,12 +51,12 @@ our $VERSION = "1.0";
 our $VERBOSE = 0;
 our $DEBUG = 0;
 
-our ($DB_FILE, $DEST_DIR, $VERSION_FILE);
+our ($DB_DIR, $DEST_DIR, $VERSION_FILE);
 our $DEF_VERSION_FILE = 'version.txt';
 
 use Getopt::Long;
 GetOptions(
-    'db-file=s'   => \$DB_FILE,
+    'db-dir=s'    => \$DB_DIR,
     'dest-dir=s'  => \$DEST_DIR,
     'vers-file=s' => \$VERSION_FILE,
     'debug'       => \$DEBUG,
@@ -68,8 +68,8 @@ GetOptions(
 #
 # Check that database file is supplied and is valid
 #
-! $DB_FILE and &usage("Command-line argument --db-file must be supplied.");
-! isFile($DB_FILE) and die("File '$DB_FILE' not found.");
+! $DB_DIR and &usage("Command-line argument --db-dir must be supplied.");
+! isPath($DB_DIR) and die("DB directory '$DB_DIR' not found.");
 
 # Check that destination directory is valid (if provided, else use the current directory)
 #
@@ -77,24 +77,30 @@ GetOptions(
 ! $DEST_DIR and $DEST_DIR = getcwd();
 $DEBUG and print STDERR "Destination Directory: $DEST_DIR\n";
 
-# Copy the file (if --dest-dir not provided, current directory)
-#
-copy($DB_FILE, $DEST_DIR) or die("File copy '$DB_FILE' to '$DEST_DIR' failed.");
-
-# Update the version file (use the default if not supplied)
+# Process the version file (use the default if not supplied)
 #
 my $version_file = $VERSION_FILE ? $VERSION_FILE : $DEF_VERSION_FILE;
 my $version_path = qq|$DEST_DIR/$version_file|;
 ! isFile($version_path) and die("Version file '$version_path' not found.");
 open(my $rfh, $version_path) or die("Unable to open '$version_path' for reading.");
-my ($version, $update) = map{ trim_all($_); $_ } split('-', <$rfh>);
+my ($db_name, $db_ext, $version, $update) = map{ trim($_); $_ } split('-', <$rfh>);
 close $rfh;
 
-$update+=1;
-$DEBUG and print STDERR "Database Version=$version, New Update=$update\n";
+# Copy the file (if --dest-dir not provided, current directory)
+#
+my $db_file = qq|$DB_DIR/$db_name\.$db_ext|;
+! isFile($db_file) and die("DB File '$db_file' not found.");
+copy($db_file, $DEST_DIR) or die("File copy '$db_file' to '$DEST_DIR' failed.");
 
+# Update the version
+#
+$update+=1;
+$DEBUG and print STDERR "DB Name=$db_name, DB Ext=$db_ext, DB Version=$version, New Update=$update\n";
+
+# Re-construct the version file
+#
 open(my $wfh, '>', $version_path) or die("Unable to open '$version_path' for writting.");
-print $wfh qq|$version-$update\n|;
+print $wfh qq|$db_name-$db_ext-$version-$update\n|;
 close $wfh;
 
 
@@ -107,7 +113,7 @@ sub usage {
     $error and print STDERR "Error: $error\n";
 
     print STDERR <<_USAGE;
-Usage:   ./$COMMAND --db-file "Database File" [ --debug --verbose ]
+Usage:   ./$COMMAND --db-dir "Database directory" [ --debug --verbose ]
 Example: ./$COMMAND --db-file ""
 _USAGE
 

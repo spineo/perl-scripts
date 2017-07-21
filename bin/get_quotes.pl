e285c3a11bb9b6b173a60ab81e1cd94e71dc7673
Stuart Pineo <svpineo@gmail.com>
2017-07-21 06:43:36 -0400

    Completed most of the quotes parsing logic and cleanup.

--- bin/get_quotes.pl
+++ bin/get_quotes.pl
@@ -59,17 +59,21 @@ our $VERSION = "1.0";
 our $VERBOSE = 0;
 our $DEBUG   = 0;
 
-our ($URL);
+our ($URL, $QUOTE_OPEN, $QUOTE_CLOSE);
 
 use Getopt::Long;
 GetOptions(
-    'url=s'       => \$URL,
-    'debug'       => \$DEBUG,
-    'verbose'     => \$VERBOSE,
-    'help|usage'  => \&usage,
+    'url=s'         => \$URL,
+    'quote-open=s'  => \$QUOTE_OPEN,
+    'quote-close=s' => \$QUOTE_CLOSE,
+    'debug'         => \$DEBUG,
+    'verbose'       => \$VERBOSE,
+    'help|usage'    => \&usage,
 );
 
-! $URL and usage("--url must be set");
+! $URL         and usage("--url must be set");
+! $QUOTE_OPEN  and usage("--quote-open must be set");
+! $QUOTE_CLOSE and usage("--quote-close must be set");
 
 # Retrieve all installations from root
 #
@@ -77,8 +81,38 @@ my $quotes_page = get $URL;
 die("Unable to retrieve content from '$URL'") unless $quotes_page;
 
 my @lines = split(/\n/, $quotes_page);
+my $qopen = 0;
+my $qtext;
 foreach my $line (@lines) {
-    print STDOUT "$line\n";
+    if ($qopen) {
+	if ($line =~ m/([^<]*)</) {
+		$qtext .= &cleanup($1);
+    		print STDOUT "$qtext\n";
+		$qtext = "";
+		$qopen = 0;
+
+	} else {
+		$qtext .= &cleanup($line);
+	}
+
+    } elsif ($line =~ m/$QUOTE_OPEN(.*)/) {
+	$qopen = 1;
+	$qtext = &cleanup($1);
+    }
+}
+
+#------------------------------------------------------------------------------
+# cleanup: Cleanup quotes text
+#------------------------------------------------------------------------------
+
+sub cleanup {
+	my $text = shift;
+
+	$text =~ s/\n/ /g;
+	$text =~ s/\&[a-z]+\;//g;
+	$text =~ s/ +/ /g;
+
+	return $text;
 }
 
 #------------------------------------------------------------------------------
@@ -88,11 +122,11 @@ foreach my $line (@lines) {
 sub usage {
     my $err = shift;
 
-    $err and print STDERR "Error: $err\n";
+    $err and print STDERR "$err\n";
 
     print STDERR <<_USAGE;
 Usage:   ./$COMMAND --url <quotes page> [ --debug --verbose ] > output_file
-Example: ./$COMMAND --url http://www.goodreads.com/quotes/tag/love?page=1 > quotes.txt
+Example: ./$COMMAND --url http://www.goodreads.com/quotes/tag/love?page=1 --quote-open '"quoteText">' --quote-close '<' > quotes.txt
 _USAGE
 
     exit(1);
